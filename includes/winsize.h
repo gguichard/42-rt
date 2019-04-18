a488eae425f3e10b962e9c0c6de14339e514faf6
Robin DUQUENNE <roduquen@student.42.fr>
2019-04-18 15:16:31 +0200

    Camera OK et termine touche OK

--- includes/winsize.h
+++ includes/winsize.h
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/31 11:13:38 by gguichar          #+#    #+#             */
-/*   Updated: 2019/04/17 11:17:03 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/04/18 15:14:21 by roduquen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define WINSIZE_H
 
 # define WIN_TITLE "RTv1"
-# define WIN_WIDTH 1280
+# define WIN_WIDTH 960
 # define WIN_HEIGHT 960
 
 typedef struct	s_winsize
--- src/key_event.c
+++ src/key_event.c
@@ -6,7 +6,7 @@
 /*   By: roduquen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 11:55:24 by roduquen          #+#    #+#             */
-/*   Updated: 2019/04/18 14:52:38 by roduquen         ###   ########.fr       */
+/*   Updated: 2019/04/18 15:06:38 by roduquen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@
 
 void		camera_event(SDL_Event *event, t_camera *camera)
 {
-	if (event->key.keysym.sym == SDLK_8)
+	if (event->key.keysym.sym == SDLK_KP_4)
 		rotate_camera(camera, (t_vec3d){0, 1, 0}, M_PI / 90);
-	if (event->key.keysym.sym == SDLK_2)
+	if (event->key.keysym.sym == SDLK_KP_6)
 		rotate_camera(camera, (t_vec3d){0, 1, 0}, -1 * M_PI / 90);
-	if (event->key.keysym.sym == SDLK_6)
+	if (event->key.keysym.sym == SDLK_KP_2)
 		rotate_camera(camera, (t_vec3d){1, 0, 0}, M_PI / 90);
-	if (event->key.keysym.sym == SDLK_4)
+	if (event->key.keysym.sym == SDLK_KP_8)
 		rotate_camera(camera, (t_vec3d){1, 0, 0}, -1 * M_PI / 90);
-	if (event->key.keysym.sym == SDLK_7)
+	if (event->key.keysym.sym == SDLK_KP_7)
 		rotate_camera(camera, (t_vec3d){0, 0, 1}, M_PI / 90);
-	if (event->key.keysym.sym == SDLK_3)
+	if (event->key.keysym.sym == SDLK_KP_3)
 		rotate_camera(camera, (t_vec3d){0, 0, 1}, -1 * M_PI / 90);
 	if (event->key.keysym.sym == SDLK_UP)
 		camera->origin = vec3d_add(camera->origin, camera->direction);

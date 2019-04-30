bfbca532cd2f93a3c8a1cb64c7f1acbadd3c12de
Gwennael Guichard <gguichar@student.42.fr>
2019-04-30 07:34:21 +0200

    Normale du cone et de l'hyperboloid

--- src/shapes/cone.c
+++ src/shapes/cone.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 16:25:25 by gguichar          #+#    #+#             */
-/*   Updated: 2019/04/28 16:28:16 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/04/30 07:29:12 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@
 
 t_vec3d	get_cone_normal(t_ray_object *object, t_vec3d intersect)
 {
-	(void)object;
-	intersect.z = .0;
+	intersect.z = -pow(tan(object->angle), 2);
 	return (vec3d_unit(intersect));
 }
 
 double	get_cone_dist(t_ray_object *object, t_vec3d origin, t_vec3d direction)
 {
 	t_quad	quad;
-	double	cos_r2;
+	double	tan_r2;
 
-	cos_r2 = pow(cos(object->angle), 2);
-	quad.a = pow(direction.z, 2) - cos_r2;
-	quad.b = 2 * (direction.z * origin.z - vec3d_dot(direction, origin)
-			* cos_r2);
-	quad.c = pow(origin.z, 2) - vec3d_length2(origin) * cos_r2;
+	tan_r2 = pow(tan(object->angle), 2);
+	quad.a = pow(direction.x, 2) + pow(direction.y, 2)
+		- pow(direction.z, 2) * tan_r2;
+	quad.b = 2 * (direction.x * origin.x + direction.y * origin.y
+			- direction.z * origin.z * tan_r2);
+	quad.c = pow(origin.x, 2) + pow(origin.y, 2) - pow(origin.z, 2) * tan_r2;
 	return (solve_quadratic_equation(&quad));
 }
--- src/shapes/hyperboloid.c
+++ src/shapes/hyperboloid.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 16:33:29 by gguichar          #+#    #+#             */
-/*   Updated: 2019/04/28 16:34:13 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/04/30 07:30:49 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@ double	get_hyperboloid_dist(t_ray_object *object, t_vec3d origin
 {
 	t_quad	quad;
 
-	quad.a = pow(direction.z, 2) - pow(direction.x, 2) - pow(direction.y, 2);
-	quad.b = 2.0 * (origin.z * direction.z - origin.x * direction.x
-			- origin.y * direction.y);
-	quad.c = pow(origin.z, 2) + object->radius - pow(origin.x, 2)
-		- pow(origin.y, 2);
+	quad.a = pow(direction.x, 2) + pow(direction.y, 2) - pow(direction.z, 2);
+	quad.b = 2 * (origin.x * direction.x + origin.y * direction.y
+			- origin.z * direction.z);
+	quad.c = pow(origin.x, 2) + pow(origin.y, 2) - pow(origin.z, 2)
+		+ object->radius;
 	return (solve_quadratic_equation(&quad));
 }
 
 t_vec3d	get_hyperboloid_normal(t_ray_object *object, t_vec3d intersect)
 {
 	(void)object;
-	intersect.z = .0;
+	intersect.z = -intersect.z;
 	return (vec3d_unit(intersect));
 }

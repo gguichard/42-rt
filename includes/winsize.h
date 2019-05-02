68a4971d13109146917efc8518c149631a8dc2a9
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-05-02 18:10:35 +0200

    Modif nom de la fenetre

--- includes/winsize.h
+++ includes/winsize.h
@@ -6,14 +6,14 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/31 11:13:38 by gguichar          #+#    #+#             */
-/*   Updated: 2019/04/27 11:41:03 by roduquen         ###   ########.fr       */
+/*   Updated: 2019/05/02 18:10:26 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WINSIZE_H
 # define WINSIZE_H
 
-# define WIN_TITLE "RTv1"
+# define WIN_TITLE "RT"
 # define WIN_WIDTH 1280
 # define WIN_HEIGHT 800
 

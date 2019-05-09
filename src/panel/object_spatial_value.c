8646e83ae7a18fa433fdd8319bf99ac6a3465bb4
YassineMekraoui <yassinemekraoui@gmail.com>
2019-05-09 11:18:53 +0200

    Display du vecteur direction de la camera au lieu des angles. Display de la rotation des objets

--- src/panel/object_spatial_value.c
+++ src/panel/object_spatial_value.c
@@ -6,7 +6,7 @@
 /*   By: ymekraou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/07 05:43:46 by ymekraou          #+#    #+#             */
-/*   Updated: 2019/05/08 06:38:47 by ymekraou         ###   ########.fr       */
+/*   Updated: 2019/05/09 11:08:37 by ymekraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,25 @@
 #include "panel.h"
 #include "vec3d.h"
 #include "raytracer.h"
+
+void	draw_obj_rot_value(t_text *msg, t_ray_object *obj)
+{
+	msg->pos.x = 477;
+	msg->pos.y = 77;
+	msg->str = ft_sprintf("%.1lf", obj->rotation.x);
+	write_text(msg, 11);
+	free(msg->str);
+	msg->pos.x = 565;
+	msg->pos.y = 77;
+	msg->str = ft_sprintf("%.1lf", obj->rotation.y);
+	write_text(msg, 11);
+	free(msg->str);
+	msg->pos.x = 653;
+	msg->pos.y = 77;
+	msg->str = ft_sprintf("%.1lf", obj->rotation.z);
+	write_text(msg, 11);
+	free(msg->str);
+}
 
 void	draw_obj_pos_value(t_text *msg, t_ray_object *obj)
 {
@@ -43,4 +62,5 @@ void	draw_obj_spatial(t_data *data)
 	set_rgba_text(&(msg.bg_color), 0xE1E1E1);
 	set_rgba_text(&(msg.fg_color), 0x0);
 	draw_obj_pos_value(&(msg), data->current);
+	draw_obj_rot_value(&(msg), data->current);
 }
--- src/panel/panel_camera_value.c
+++ src/panel/panel_camera_value.c
@@ -6,7 +6,7 @@
 /*   By: ymekraou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/03 06:27:43 by ymekraou          #+#    #+#             */
-/*   Updated: 2019/05/09 09:42:14 by ymekraou         ###   ########.fr       */
+/*   Updated: 2019/05/09 10:56:14 by ymekraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,48 +35,21 @@ void	draw_camera_pos_value(t_text *msg, t_camera *cam)
 	free(msg->str);
 }
 
-void	compute_cam_angle(double angle[3], t_camera *cam)
-{
-	t_vec3d		axe;
-	double		res;
-
-	axe = vec3d(1, 0, 0);
-	axe = vec3d_unit(axe);
-	res = vec3d_dot(cam->direction, axe);
-	angle[0] = acos(res) * (180.0 / M_PI);
-//	if (cam->direction.z > 0)
-//		angle[0] *= (-1); 
-	axe = vec3d(0, 1, 0);
-	axe = vec3d_unit(axe);
-	res = vec3d_dot(cam->up, axe);
-	angle[1] = acos(res) * (180.0 / M_PI);
-//	if (cam->direction.z < 0)
-//		angle[1] *= (-1); 
-	axe = vec3d(0, 0, 1);
-	res = vec3d_dot(cam->up, axe);
-	angle[2] = acos(res) * (180.0 / M_PI);
-//	if (cam->direction.y < 0)
-//		angle[2] *= (-1); 
-}
-
 void	draw_camera_angle_value(t_text *msg, t_camera *cam)
 {
-	double	angle[3];
-
-	compute_cam_angle(angle, cam);
 	msg->pos.x = 275;
 	msg->pos.y = 65;
-	msg->str = ft_sprintf("%.1lf", angle[0]);
+	msg->str = ft_sprintf("%.1lf", cam->direction.x);
 	write_text(msg, 12);
 	free(msg->str);
 	msg->pos.x = 275;
 	msg->pos.y = 87;
-	msg->str = ft_sprintf("%.1lf", angle[1]);
+	msg->str = ft_sprintf("%.1lf", cam->direction.y);
 	write_text(msg, 12);
 	free(msg->str);
 	msg->pos.x = 275;
 	msg->pos.y = 110;
-	msg->str = ft_sprintf("%.1lf", angle[2]);
+	msg->str = ft_sprintf("%.1lf", cam->direction.z);
 	write_text(msg, 12);
 	free(msg->str);
 }

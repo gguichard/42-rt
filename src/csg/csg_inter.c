bc05f136c8b954ccd5b1291a3446f079308f2c3b
Robin DUQUENNE <roduquen@student.42.fr>
2019-05-10 20:09:33 +0200

    csg all normed

--- src/csg/csg_inter.c
+++ src/csg/csg_inter.c
@@ -6,12 +6,74 @@
 /*   By: roduquen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 20:03:27 by roduquen          #+#    #+#             */
-/*   Updated: 2019/05/10 19:32:22 by roduquen         ###   ########.fr       */
+/*   Updated: 2019/05/10 20:09:08 by roduquen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ray_object.h"
 #include "vec3d.h"
+
+static void	csg_inter_utils(t_ray_hit hit1, t_ray_hit hit2, t_ray_hit *hit)
+{
+	hit->dist = hit1.dist;
+	hit->normal = hit1.normal;
+	hit->dist_b = hit2.dist;
+	hit->normal_b = hit2.normal;
+}
+
+static void	csg_inter_func_2(t_ray_hit hit1, t_ray_hit hit2, t_ray_hit *hit)
+{
+	if (hit2.dist == hit2.dist_b)
+	{
+		if (hit2.dist < hit1.dist)
+		{
+			hit->dist = hit2.dist;
+			hit->normal = hit2.normal;
+			hit->dist_b = -1;
+		}
+		else
+			csg_inter_utils(hit1, hit2, hit);
+	}
+	else
+	{
+		if (hit2.dist < hit1.dist && hit2.dist_b < hit1.dist)
+			*hit = hit2;
+		else if (hit2.dist < hit1.dist)
+			csg_inter_utils(hit2, hit1, hit);
+		else
+		{
+			hit->dist = -1;
+			hit->dist_b = -1;
+		}
+	}
+}
+
+static void	csg_inter_func_3(t_ray_hit hit1, t_ray_hit hit2, t_ray_hit *hit)
+{
+	if (hit2.dist_b < hit1.dist || hit2.dist >= hit1.dist_b)
+	{
+		hit->dist = -1;
+		hit->dist_b = -1;
+	}
+	else if (hit1.dist < hit2.dist && hit2.dist_b > hit1.dist_b)
+	{
+		hit->dist = hit2.dist;
+		hit->normal = hit2.normal;
+		hit->dist_b = hit1.dist_b;
+		hit->normal_b = hit1.normal_b;
+	}
+	else if (hit2.dist < hit1.dist && hit2.dist_b < hit1.dist_b)
+	{
+		hit->dist = hit1.dist;
+		hit->normal = hit1.normal;
+		hit->dist_b = hit2.dist_b;
+		hit->normal_b = hit2.normal_b;
+	}
+	else if (hit2.dist_b < hit1.dist_b)
+		*hit = hit2;
+	else
+		*hit = hit1;
+}
 
 void		csg_inter_func(t_ray_hit hit1, t_ray_hit hit2, t_ray_hit *hit)
 {
@@ -21,41 +83,7 @@ void		csg_inter_func(t_ray_hit hit1, t_ray_hit hit2, t_ray_hit *hit)
 		hit->dist_b = -1;
 	}
 	else if (hit1.dist == hit1.dist_b)
-	{
-		if (hit2.dist == hit2.dist_b)
-		{
-			if (hit2.dist < hit1.dist)
-			{
-				hit->dist = hit2.dist;
-				hit->normal = hit2.normal;
-				hit->dist_b = -1;
-			}
-			else
-			{
-				hit->dist = hit1.dist;
-				hit->normal = hit1.normal;
-				hit->dist_b = hit2.dist;
-				hit->normal_b = hit2.normal;
-			}
-		}
-		else
-		{
-			if (hit2.dist < hit1.dist && hit2.dist_b < hit1.dist)
-				*hit = hit2;
-			else if (hit2.dist < hit1.dist)
-			{
-				hit->dist = hit2.dist;
-				hit->normal = hit2.normal;
-				hit->dist_b = hit1.dist;
-				hit->normal_b = hit1.normal;
-			}
-			else
-			{
-				hit->dist = -1;
-				hit->dist_b = -1;
-			}
-		}
-	}
+		csg_inter_func_2(hit1, hit2, hit);
 	else
 	{
 		if (hit2.dist == hit2.dist_b)
@@ -66,40 +94,11 @@ void		csg_inter_func(t_ray_hit hit1, t_ray_hit hit2, t_ray_hit *hit)
 				hit->dist_b = -1;
 			}
 			else if (hit2.dist > hit1.dist && hit2.dist < hit1.dist_b)
-			{
-				hit->dist = hit1.dist;
-				hit->normal = hit1.normal;
-				hit->dist_b = hit2.dist;
-				hit->normal_b = hit2.normal;
-			}
+				csg_inter_utils(hit1, hit2, hit);
 			else
 				*hit = hit1;
 		}
 		else
-		{
-			if (hit2.dist_b < hit1.dist || hit2.dist >= hit1.dist_b)
-			{
-				hit->dist = -1;
-				hit->dist_b = -1;
-			}
-			else if (hit1.dist < hit2.dist && hit2.dist_b > hit1.dist_b)
-			{
-				hit->dist = hit2.dist;
-				hit->normal = hit2.normal;
-				hit->dist_b = hit1.dist_b;
-				hit->normal_b = hit1.normal_b;
-			}
-			else if (hit2.dist < hit1.dist &&  hit2.dist_b < hit1.dist_b)
-			{
-				hit->dist = hit1.dist;
-				hit->normal = hit1.normal;
-				hit->dist_b = hit2.dist_b;
-				hit->normal_b = hit2.normal_b;
-			}
-			else if (hit2.dist_b < hit1.dist_b)
-				*hit = hit2;
-			else
-				*hit = hit1;
-		}
+			csg_inter_func_3(hit1, hit2, hit);
 	}
 }
